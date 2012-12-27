1c1691cee188cd86c6308e54650af5b3469ceeeb
Nicolas McCurdy <thenickperson@gmail.com>
2012-12-27 16:46:22 -0500

    Syntax tweaks

--- app.rb
+++ app.rb
@@ -5,9 +5,9 @@ require_relative 'ticket'
 class App < Sinatra::Base
 
 	# If you want the logs displayed you have to do this before the call to setup
-	DataMapper::Logger.new($stdout, :debug)
+	DataMapper::Logger.new $stdout, :debug
 	# A Sqlite3 connection to a persistent database
-	DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/tickets.db")
+	DataMapper::setup :default, "sqlite3://#{Dir.pwd}/tickets.db"
 	# Finalize models
 	DataMapper.finalize
 
