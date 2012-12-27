2182f38fe616c73aaffb4b8580a555568df335df
Nicolas McCurdy <thenickperson@gmail.com>
2012-12-26 19:02:29 -0500

    Support POST requests for creating tickets and adding them to a queue

--- app.rb
+++ app.rb
@@ -3,10 +3,17 @@ require_relative 'ticket'
 
 class App < Sinatra::Base
 
+	queue = []
+
 	# Pages
 
 	get '/' do
 		erb :index
+	end
+
+	post '/' do
+		queue << Ticket.new(params[:name])
+		"Your ticket has been filed under the name \"#{params[:name]}.\""
 	end
 
 	get '/tickets' do
