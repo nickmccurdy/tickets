b08909d22766b47ecff2d91cf0ce178982232d5c
Nicolas McCurdy <thenickperson@gmail.com>
2012-12-26 19:14:01 -0500

    Add @resolved to Ticket objects and let Tickets print and modify their statuses

--- ticket.rb
+++ ticket.rb
@@ -1,14 +1,19 @@
 class Ticket
 
-	attr_reader :name, :created
+	attr_reader :name, :created, :resolved
 
 	def initialize name
 		@name = name
 		@created = Time.now
+		@resolved = false
+	end
+
+	def resolve
+		@resolved = true
 	end
 
 	def to_s
-		"#{@name} at #{@created}"
+		"#{@name} at #{@created} [#{@resolved? 'resolved' : 'pending'}]"
 	end
 
 end
