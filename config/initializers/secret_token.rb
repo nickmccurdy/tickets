c27175fe71b1b4a869d8fb262cd592e2587e2cf5
Nicolas McCurdy <thenickperson@gmail.com>
2012-12-30 20:55:36 -0500

    Create a new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tickets::Application.config.secret_token = '355fc0cef03a6da6a4cb199c1770aab8a46b4a22a7ae54e0a14ffe2f1ba7f4478a067a8e774c5ce76158f268632de2e2357793a7f2177b3e31aab71f406cb3c6'
