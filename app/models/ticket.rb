ad81e966c7614af8ee2f482ecf6223fa51539e68
Nicolas McCurdy <thenickperson@gmail.com>
2012-12-31 02:04:22 -0500

    Migrate code from the old Ticket model to the new one

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,16 @@
 class Ticket < ActiveRecord::Base
-  attr_accessible :open, :owner, :reason
+	attr_accessible :open, :owner, :reason
+
+	after_initialize :set_defaults
+
+	validates :owner, :presence => true
+	# Try to also ensure that the name is not empty
+
+	def set_defaults
+		open ||= true
+	end
+
+	def to_s
+		"#{name} at #{reated_at} for #{reason}"
+	end
 end
