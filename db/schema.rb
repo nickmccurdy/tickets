f82f9bea06038d1807e4976dee898280c82a1bdc
Nicolas McCurdy <thenickperson@gmail.com>
2012-12-31 22:02:47 -0500

    Update database schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121231041021) do
+ActiveRecord::Schema.define(:version => 20130101025303) do
 
   create_table "tickets", :force => true do |t|
     t.string   "owner"
     t.string   "reason"
-    t.boolean  "open"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
