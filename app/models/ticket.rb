d208643bd487c9a4a4c9d29ccf180ae38b1027e0
Nicolas McCurdy <thenickperson@gmail.com>
2012-12-31 22:06:40 -0500

    Remove the open column for Tickets within the appropriate models and views

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,14 +1,8 @@
 class Ticket < ActiveRecord::Base
-	attr_accessible :open, :owner, :reason
-
-	after_initialize :set_defaults
+	attr_accessible :owner, :reason
 
 	validates :owner, :presence => true
 	# Try to also ensure that the name is not empty
-
-	def set_defaults
-		open ||= true
-	end
 
 	def to_s
 		"#{name} at #{created_at} for #{reason}"
