e59af5234444442b30e84fc0ae9465a5e23dc38b
Nicolas McCurdy <thenickperson@gmail.com>
2013-01-02 02:32:53 -0500

    Remove my Ticket#to_s method

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,8 +3,4 @@ class Ticket < ActiveRecord::Base
 
 	validates :computer, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 21 }, uniqueness: { scope: :reason, message: 'only one ticket per reason per computer is allowed' }
 	validates :reason, presence: true, inclusion: ['help', 'lab completed']
-
-	def to_s
-		"#{name} at #{created_at} for #{reason}"
-	end
 end
