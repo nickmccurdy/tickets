8efe0f094de785b273b134834351fc82ac5e5343
Nicolas McCurdy <thenickperson@gmail.com>
2013-01-02 02:32:32 -0500

    Fix validation for computer number

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,7 +1,7 @@
 class Ticket < ActiveRecord::Base
 	attr_accessible :computer, :reason
 
-	validates :computer, presence: true, numericality: true, inclusion: 1..21, uniqueness: { scope: :reason, message: 'only one ticket per reason per computer is allowed' }
+	validates :computer, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 21 }, uniqueness: { scope: :reason, message: 'only one ticket per reason per computer is allowed' }
 	validates :reason, presence: true, inclusion: ['help', 'lab completed']
 
 	def to_s
