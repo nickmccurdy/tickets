4f8f057cfcd185d784b4e05876cf6e5b8192aa3a
Nicolas McCurdy <thenickperson@gmail.com>
2013-01-02 01:51:11 -0500

    Validate ALL the things!

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,8 +1,8 @@
 class Ticket < ActiveRecord::Base
 	attr_accessible :computer, :reason
 
-	validates :computer, presence: true
-	# Try to also ensure that the computer number is not empty
+	validates :computer, presence: true, numericality: true, inclusion: 1..21, uniqueness: { scope: :reason, message: 'only one ticket per reason per computer is allowed' }
+	validates :reason, presence: true, inclusion: ['help', 'lab completed']
 
 	def to_s
 		"#{name} at #{created_at} for #{reason}"
