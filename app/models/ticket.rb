caa5eef732d19bc537e5ffc2d06c8f8271e66869
Nicolas McCurdy <thenickperson@gmail.com>
2013-01-04 02:08:43 -0500

    Update validations so only one ticket is allowed per computer (fix #20)

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,6 +3,6 @@ class Ticket < ActiveRecord::Base
 
 	default_scope order 'created_at ASC'
 
-	validates :computer, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 21 }, uniqueness: { scope: :reason, message: 'only one ticket per reason per computer is allowed' }
+	validates :computer, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 21 }, uniqueness: { message: 'only one ticket per computer is allowed' }
 	validates :reason, presence: true, inclusion: ['help', 'lab completed']
 end
