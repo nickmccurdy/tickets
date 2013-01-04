988835846f22a65e37aa9a178000bfaa394807ff
Nicolas McCurdy <thenickperson@gmail.com>
2013-01-03 23:57:32 -0500

    Sort the ticket list from oldest to newest

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,8 @@
 class Ticket < ActiveRecord::Base
 	attr_accessible :computer, :reason
 
+	default_scope order 'created_at ASC'
+
 	validates :computer, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 21 }, uniqueness: { scope: :reason, message: 'only one ticket per reason per computer is allowed' }
 	validates :reason, presence: true, inclusion: ['help', 'lab completed']
 end
