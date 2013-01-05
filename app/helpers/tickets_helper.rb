5165ef801e840faea018fb5e8041d85240c063a8
Nicolas McCurdy <thenickperson@gmail.com>
2013-01-05 02:51:32 -0500

    Add path_contains? helper

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,5 +1,9 @@
 module TicketsHelper
 
+	def path_contains? str
+		request.path.include? str
+	end
+
 	def navbar_link name, path, icon=nil
 		render partial: 'navbar_link', locals: {
 			path: path,
