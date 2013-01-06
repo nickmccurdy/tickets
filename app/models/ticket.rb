d4877b59784140b79e3a7f0a5fffb520ed2c7eaa
Nicolas McCurdy <thenickperson@gmail.com>
2013-01-06 14:39:42 -0500

    Write Ticket#position for checking the position of a ticket after sorting

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -6,4 +6,8 @@ class Ticket < ActiveRecord::Base
 	validates :name, presence: true
 	validates :computer, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 21 }, uniqueness: true
 	validates :reason, presence: true, inclusion: ['help', 'lab completed']
+
+	def position
+		Ticket.where('created_at <= (?)', created_at).count
+	end
 end
