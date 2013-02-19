b7fb3b6002d40ab8644d4739e1ad0501c84d6e82
Nicolas McCurdy <thenickperson@gmail.com>
2013-02-19 03:33:44 -0500

    Clean up validation code in the Ticket model and simplify usage of Rails syntax

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -49,7 +49,7 @@ class TicketsController < ApplicationController
 				format.html { redirect_to '/' }
 				format.json { render json: @ticket, status: :created, location: @ticket }
 			else
-				format.html { render action: "new" }
+				format.html { render "new" }
 				format.json { render json: @ticket.errors, status: :unprocessable_entity }
 			end
 		end
@@ -65,7 +65,7 @@ class TicketsController < ApplicationController
 				format.html { redirect_to @ticket, notice: 'Ticket was successfully updated.' }
 				format.json { head :no_content }
 			else
-				format.html { render action: "edit" }
+				format.html { render "edit" }
 				format.json { render json: @ticket.errors, status: :unprocessable_entity }
 			end
 		end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,9 +3,15 @@ class Ticket < ActiveRecord::Base
 
 	default_scope order 'created_at ASC'
 
-	validates :name, presence: true
-	validates :computer, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 21 }, uniqueness: true
-	validates :reason, presence: true, inclusion: ['help', 'lab completed']
+	validates :name,
+		presence: true
+	validates :computer,
+		presence: true,
+		numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 21 },
+		uniqueness: true
+	validates :reason,
+		presence: true,
+		inclusion: ['help', 'lab completed']
 
 	def position
 		Ticket.where('created_at <= (?)', created_at).count
