42ee9c6d1111fb34fcfb70649da8862f699f4f91
Nicolas McCurdy <thenickperson@gmail.com>
2013-02-23 11:16:38 -0500

    Write tests for Ticket and ApplicationHelper

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -1,4 +1,18 @@
 require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
+
+  test 'should check if a path matches the path of the current page' do
+    fail
+  end
+
+  test 'should render a navbar link (smoke test)' do
+    assert_not_nil navbar_link 'Example', 'http://example.com'
+    assert_not_nil navbar_link 'Example', 'http://example.com', 'home'
+  end
+
+  test 'should render an attribute (smoke test)' do
+    assert_not_nil show_attribute 'Name', 'Value'
+  end
+
 end
--- test/unit/ticket_test.rb
+++ test/unit/ticket_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class TicketTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @ticket1 = tickets :one
+    @ticket2 = tickets :two
+  end
+
+  test 'should be sorted by creation time (ascending)' do
+    assert_equal Ticket.all, [@ticket1, @ticket2]
+  end
+
+  test 'should validate name' do
+    Ticket.create name: 'Bob', computer: 1, reason: 'help'
+    assert_raise { Ticket.create computer: 2, reason: 'help', name: '' }
+    assert_raise { Ticket.create computer: 3, reason: 'help' }
+  end
+
+  test 'should validate computer' do
+    Ticket.create name: 'Bob', computer: 1, reason: 'help'
+    Ticket.create name: 'Bob', computer: 10, reason: 'help'
+    Ticket.create name: 'Bob', computer: 21, reason: 'help'
+    assert_raise { Ticket.create name: 'Bob', reason: 'help' }
+    assert_raise { Ticket.create name: 'Bob', computer: 1, reason: 'help' }
+    assert_raise { Ticket.create name: 'Bob', computer: 'two', reason: 'help' }
+    assert_raise { Ticket.create name: 'Bob', computer: 2.0, reason: 'help' }
+    assert_raise { Ticket.create name: 'Bob', computer: 0, reason: 'help' }
+    assert_raise { Ticket.create name: 'Bob', computer: -10, reason: 'help' }
+    assert_raise { Ticket.create name: 'Bob', computer: 22, reason: 'help' }
+  end
+
+  test 'should validate reason' do
+    Ticket.create name: 'Bob', computer: 1, reason: 'help'
+    Ticket.create name: 'Bob', computer: 2, reason: 'lab completed'
+    assert_raise { Ticket.create name: 'Bob', computer: 2 }
+    assert_raise { Ticket.create name: 'Bob', computer: 2, reason: '' }
+    assert_raise { Ticket.create name: 'Bob', computer: 2, reason: 'yolo' }
+  end
+
+  test 'should get its position in the queue' do
+    assert_equal @ticket1.position, 0
+    assert_equal @ticket2.position, 1
+  end
+
 end
