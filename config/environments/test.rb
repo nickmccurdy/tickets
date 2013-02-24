cab637d0a4f2fdfdc7571fee3b5530b9b4be1bec
Nicolas McCurdy <thenickperson@gmail.com>
2013-02-24 16:11:45 -0500

    Set config.eager_load for all environments

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,6 +11,8 @@ Tickets::Application.configure do
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
 
+  # Eager loading
+  config.eager_load = false
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
