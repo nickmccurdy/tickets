7f133872ad27ed66d39994e095f1b9c1121d3a4f
Nicolas McCurdy <thenickperson@gmail.com>
2013-02-24 16:56:55 -0500

    Use secret_key_base and :upgrade_signature_to_encryption_cookie_store

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,5 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Tickets::Application.config.secret_token = '355fc0cef03a6da6a4cb199c1770aab8a46b4a22a7ae54e0a14ffe2f1ba7f4478a067a8e774c5ce76158f268632de2e2357793a7f2177b3e31aab71f406cb3c6'
+
+Tickets::Application.config.secret_key_base = 'some secret'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,3 +6,5 @@ Tickets::Application.config.session_store :cookie_store, key: '_tickets_session'
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # Tickets::Application.config.session_store :active_record_store
+
+Tickets::Application.config.session_store :upgrade_signature_to_encryption_cookie_store, key: '_tickets_session'
