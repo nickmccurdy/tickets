e0f73fee976af9c9a75f8073a6d5b17bdc6cdac0
Nicolas McCurdy <thenickperson@gmail.com>
2013-02-28 00:16:58 -0500

    Remove partials, helper methods, and tests for navbar links (they're not in use)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,6 @@
 # Provides helper methods for the application's models, views, and controllers.
 module ApplicationHelper
 
-  # Renders a Bootstrap-ified navbar link with an optional icon.
-  #
-  # @param [String] name the name of the link, which is displayed to the user
-  # @param [String] path the path that the link should bring the user to
-  # @param [String] icon the name of the Bootstrap icon to display with the link
-  # (excluding "icon-")
-  #
-  # @return [String] the generated HTML navbar link
-  def navbar_link name, path, icon=nil
-    render 'tickets/navbar_link', path: path, name: name, icon: icon
-  end
-
   # Renders a nice display of an attribute with a given name and value.
   #
   # @param [String] name the name of the attribute
--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -2,11 +2,6 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
 
-  test 'should render a navbar link (smoke test)' do
-    assert_not_nil navbar_link 'Example', 'http://example.com'
-    assert_not_nil navbar_link 'Example', 'http://example.com', 'home'
-  end
-
   test 'should render an attribute (smoke test)' do
     assert_not_nil show_attribute 'Name', 'Value'
   end
