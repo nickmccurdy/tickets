9ffd9ff8371331fb0231b622d6bc41569c806117
Nicolas McCurdy <thenickperson@gmail.com>
2013-02-28 02:06:04 -0500

    Replace many config files with those from a brand new Rails 4 app

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,8 +2,11 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Tickets::Application.config.secret_token = '355fc0cef03a6da6a4cb199c1770aab8a46b4a22a7ae54e0a14ffe2f1ba7f4478a067a8e774c5ce76158f268632de2e2357793a7f2177b3e31aab71f406cb3c6'
+# You can use `rake secret` to generate a secure secret key.
 
-Tickets::Application.config.secret_key_base = 'some secret'
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tickets::Application.config.secret_key_base = '9532020fd625e4defa4358974fe1863c2a7658e52a3667959f103cec2e47811f892ce7091ca61e45b6807c0af0e2908d9a176d85f7ba1018618e559c0217fa8d'
