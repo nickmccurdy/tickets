a975b2da3190e864163e00589f84ff901b682c9b
Nicolas McCurdy <thenickperson@gmail.com>
2013-12-26 02:50:44 -0500

    Remove the show_attribute partial and inline its functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,3 @@
 # Provides helper methods for the application's models, views, and controllers.
 module ApplicationHelper
-
-  # Renders a nice display of an attribute with a given name and value.
-  #
-  # @param [String] name the name of the attribute
-  # @param [String] value the value of the attribute
-  #
-  # @return [String] the generated HTML display of the attribute
-  def show_attribute name, value
-    render 'tickets/show_attribute', name: name, value: value
-  end
-
 end
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
-
-  test 'should render an attribute (smoke test)' do
-    assert_not_nil show_attribute 'Name', 'Value'
-  end
-
 end
